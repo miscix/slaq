02d9e39273240aa5fa39d0070ce86e7b4d7bf1cb
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-03-13 07:00:55 +0400

    fix(api): add essential middleware
    Includes
    
    - body-parser

--- packages/api-server/src/app.js
+++ packages/api-server/src/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const router = require('./router')
 
 //
 
 const app = express()
+
+app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', router)
 
