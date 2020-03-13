3f0e87d88d0261685a632396c13c58765bbab446
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-03-13 05:34:27 +0400

    test(api): outline routing (todo)
    Based on API blueprint

---
+++ packages/api-server/test/behavior/authorized.test.js
@@ -0,0 +1,95 @@
+const { serial: test } = require('ava')
+
+const http = require('http')
+const listen = require('test-listen')
+const got = require('got')
+
+const createApp = require('../..')
+
+test.beforeEach(async t => {
+  const app = createApp()
+  const server = http.createServer(app)
+  const baseUrl = await listen(server)
+
+  const request = got.extend({
+    prefixUrl: baseUrl,
+    responseType: 'json'
+  })
+
+  t.context = {
+    server,
+    request
+  }
+})
+
+test.afterEach.always(async t => {
+  t.context.server.close()
+})
+
+// tests
+
+// users
+
+test.todo('GET /users/:id - 200')
+test('GET /users/:id - 404', async t => {
+  const { request } = t.context
+
+  const id = 100
+
+  const { statusCode } = await request.get(`users/${id}`)
+
+  t.is(statusCode, 404)
+})
+
+test.todo('PUT /users/:id - 204')
+test.todo('PUT /users/:id - 404')
+
+test.todo('GET /users - 200')
+
+// workspaces
+
+test.todo('POST /workspaces/ - 201')
+test.todo('POST /workspaces/ - 409')
+test.todo('POST /workspaces/ - 422')
+
+test.todo('GET /workspaces/:uri - 200')
+test.todo('GET /workspaces/:uri - 403')
+test.todo('GET /workspaces/:uri - 404')
+
+test.todo('PUT /workspaces/:uri - 204')
+test.todo('PUT /workspaces/:uri - 403')
+test.todo('PUT /workspaces/:uri - 422')
+
+test.todo('HEAD /workspaces/:uri - 204')
+test.todo('HEAD /workspaces/:uri - 404')
+
+test.todo('DELETE /workspaces/:uri - 204')
+test.todo('DELETE /workspaces/:uri - 403')
+test.todo('DELETE /workspaces/:uri - 404')
+
+test.todo('GET /workspaces - 200')
+
+// workspace members
+
+test.todo('POST /workspaces/:workspaceUri/members - 204')
+test.todo('POST /workspaces/:workspaceUri/members - 403')
+test.todo('POST /workspaces/:workspaceUri/members - 409')
+
+test.todo('DELETE /workspaces/:workspaceUri/members/:userId - 204')
+test.todo('DELETE /workspaces/:workspaceUri/members/:userId - 403')
+test.todo('DELETE /workspaces/:workspaceUri/members/:userId - 404')
+
+test.todo('GET /workspaces/:workspaceUri/members/ - 200')
+test.todo('GET /workspaces/:workspaceUri/members/ - 403')
+
+// workspace channels
+
+test.todo('POST /workspaces/:workspaceUri/channels - 201')
+test.todo('POST /workspaces/:workspaceUri/channels - 403')
+test.todo('POST /workspaces/:workspaceUri/channels - 422')
+
+test.todo('DELETE /workspaces/:workspaceUri/channels/:uri - 204')
+test.todo('DELETE /workspaces/:workspaceUri/channels/:uri - 404')
+
+test.todo('GET /workspaces/:workspaceUri/channels/ - 200')
+test.todo('GET /workspaces/:workspaceUri/channels/ - 403')
---
+++ packages/api-server/test/behavior/unauthorized.test.js
@@ -0,0 +1,77 @@
+const { serial: test } = require('ava')
+
+const http = require('http')
+const listen = require('test-listen')
+const got = require('got')
+
+const createApp = require('../..')
+
+test.beforeEach(async t => {
+  const app = createApp()
+  const server = http.createServer(app)
+  const baseUrl = await listen(server)
+
+  const request = got.extend({
+    prefixUrl: baseUrl,
+    responseType: 'json'
+  })
+
+  t.context = {
+    server,
+    request
+  }
+})
+
+test.afterEach.always(async t => {
+  t.context.server.close()
+})
+
+// tests
+
+// tokens
+
+test('POST /tokens - 401', async t => {
+  const { request } = t.context
+
+  const data = {
+    email: 'wrong@email.com',
+    password: 'no-password'
+  }
+
+  const { statusCode } = await request.post('tokens', data)
+
+  t.is(statusCode, 401)
+})
+
+// users
+
+test.todo('POST /users - 201')
+test.todo('POST /users - 409')
+test.todo('POST /users - 422')
+
+test.todo('GET /users/:userId - 401')
+test.todo('PUT /users/:userId - 401')
+test.todo('GET /users - 401')
+
+// workspaces
+
+test.todo('POST /workspaces/ - 401')
+test.todo('GET /workspaces/:workspaceUri - 401')
+test.todo('PUT /workspaces/:workspaceUri - 401')
+test.todo('DELETE /workspaces/:workspaceUri - 401')
+
+test.todo('GET /workspaces - 401')
+
+// workspace members
+
+test.todo('POST /workspaces/:workspaceUri/members - 401')
+test.todo('DELETE /workspaces/:workspaceUri/members/:userId - 401')
+
+test.todo('GET /workspaces/:workspaceUri/members/ - 401')
+
+// workspace channels
+
+test.todo('POST /workspaces/:workspaceUri/channels - 401')
+test.todo('DELETE /workspaces/:workspaceUri/channels/:channelUri - 401')
+
+test.todo('GET /workspaces/:workspaceUri/channels/ - 401')
