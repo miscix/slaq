aa444837b78f0388bb706d6f90c081be512c93e3
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-03-16 02:10:22 +0400

    chore(assets): add sample workspace data

---
+++ packages/assets/data/workspaces.js
@@ -0,0 +1,30 @@
+const school = {
+  uri: 'school',
+  name: 'School',
+  createdBy: 1,
+  users: [
+    1,
+    2
+  ],
+  channels: [
+    'general',
+    'offtop'
+  ]
+}
+
+const corp = {
+  uri: 'corp',
+  name: 'Corp.',
+  createdBy: 2,
+  users: [
+    2
+  ],
+  channels: [
+    'general'
+  ]
+}
+
+module.exports = [
+  school,
+  corp
+]
